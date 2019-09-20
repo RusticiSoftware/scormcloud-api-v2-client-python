b3d1a4d257199084e5f6562856407426cf808810
josh.white <josh.white@scorm.com>
2019-09-20 13:24:32 -0500

    Bring lib out of beta

--- rustici_software_cloud_v2/apis/__init__.py
+++ rustici_software_cloud_v2/apis/__init__.py
@@ -1,8 +1,16 @@
 from __future__ import absolute_import
 
 # import apis into api package
+from .about_api import AboutApi
+from .admin_api import AdminApi
 from .application_management_api import ApplicationManagementApi
 from .authentication_api import AuthenticationApi
 from .course_api import CourseApi
+from .dispatch_api import DispatchApi
+from .invitations_api import InvitationsApi
+from .learner_api import LearnerApi
 from .ping_api import PingApi
 from .registration_api import RegistrationApi
+from .reporting_api import ReportingApi
+from .website_api import WebsiteApi
+from .xapi_api import XapiApi
